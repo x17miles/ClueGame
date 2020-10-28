d7309e03ca6c1bfcb5a665e22fa0d73ca670de94
Miles Allain <allain@mymail.mines.edu>
2020-10-28 16:31:33 -0600

    reorganized methods in BoardCell to group getters, setters, etc. together

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -16,65 +16,7 @@ public class BoardCell {
 	private DoorDirection doorDirection;
 	private Set<BoardCell> adjList;
 	
-	public void setSecretPassage(char c) {
-		this.secretPassage = c;
-		this.secret = true;
-	}
-	public boolean isSecret() {
-		return this.secret;
-	}
-	public void setRoomLabel(boolean t) {
-		this.roomLabel = t;
-	}
-	public void setRoomCenter(boolean t) {
-		this.roomCenter = t;
-	}
-	
-	public void setDoorway(DoorDirection d) {
-		this.doorDirection = d;
-	}
-	
-	public char getInitial() {
-		return initial;
-	}
-	
-	public char getSecretPassage() {
-		return secretPassage;
-	}
-	
-	public boolean isLabel() {
-		return roomLabel;
-	}
-	public boolean isRoomCenter() {
-		return roomCenter;
-	}
-	
-	public boolean isDoorway() {
-		//if it isn't none, then it is a door
-		return !doorDirection.equals(DoorDirection.NONE);
-	}
-	public DoorDirection getDoorDirection() {
-		return doorDirection;
-	}
-	public boolean isInRoom() {
-		return inRoom;
-	}
 
-	public void setInRoom(boolean inRoom) {
-		this.inRoom = inRoom;
-	}
-
-	public boolean isOccupied() {
-		return occupied;
-	}
-
-	public void setOccupied(boolean occupied) {
-		this.occupied = occupied;
-	}
-	public void addAdj(BoardCell cell) {
-		this.adjList.add(cell);
-	}
-	
 	public BoardCell(int row, int col, char initial) {
 		//initialize row and column and make the adjList an empty set for the moment
 		super();
@@ -89,8 +31,74 @@ public class BoardCell {
 		this.initial = initial;
 		doorDirection = DoorDirection.NONE;
 	}
+	
+	public void setSecretPassage(char c) {
+		this.secretPassage = c;
+		this.secret = true;
+	}
+	
+	public void setRoomLabel(boolean t) {
+		this.roomLabel = t;
+	}
+	public void setRoomCenter(boolean t) {
+		this.roomCenter = t;
+	}
+	
+	public void setDoorway(DoorDirection d) {
+		this.doorDirection = d;
+	}
+	public void setInRoom(boolean inRoom) {
+		this.inRoom = inRoom;
+	}
 
+	
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	public void addAdj(BoardCell cell) {
+		this.adjList.add(cell);
+	}
+	
+	public char getInitial() {
+		return initial;
+	}
+	
+	public char getSecretPassage() {
+		return secretPassage;
+	}
 	public Set<BoardCell> getAdjList(){
 		return adjList;
 	}
+	
+	
+	public DoorDirection getDoorDirection() {
+		return doorDirection;
+	}
+	
+
+	
+	public boolean isInRoom() {
+		return inRoom;
+	}
+	
+	public boolean isLabel() {
+		return roomLabel;
+	}
+	public boolean isRoomCenter() {
+		return roomCenter;
+	}
+	
+	public boolean isDoorway() {
+		//if it isn't none, then it is a door
+		return !doorDirection.equals(DoorDirection.NONE);
+	}
+	public boolean isOccupied() {
+		return occupied;
+	}
+	public boolean isSecret() {
+		return this.secret;
+	}
+
+
+	
 }
